6b7572a02e8558ac36000c68a304b09de50fe201
Isaac Robinson <isaac@irobinson.org>
2025-06-20 00:01:20 +1000

    Removed boilerplate code and started with basic file return on get

---
+++ Controllers/RedirectController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace LinkTracker.Controllers
+{
+    [ApiController]
+    public class RedirectController : Controller
+    {
+        [HttpGet("/{id}")]
+        public async Task<ActionResult> Get(string id)
+        {
+            MemoryStream ms = new();
+            StreamWriter writer = new(ms);
+            writer.WriteLine(id);
+            writer.Flush();
+            return File(ms, "application/text");
+        }
+    }
+}
