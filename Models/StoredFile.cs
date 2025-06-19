ffad6f22f6a98b8d077360fbab4fd51a811c1342
Isaac Robinson <isaac@irobinson.org>
2025-06-20 01:23:50 +1000

    Added upload and download for files - current with inmemory db

---
+++ Models/StoredFile.cs
@@ -0,0 +1,27 @@
+﻿
+namespace LinkTracker.Models
+{
+    public class StoredFile
+    {
+        public string Name { get; set; }
+        public string Ext { get; set; }
+        public byte[] Data { get; set; }
+        public string ContentType { get; set; }
+
+        public StoredFile(string name, string ext, Stream data, string contentType)
+        {
+            Name = name;
+            Ext = ext;
+            Data = new byte[data.Length];
+            data.Read(Data, 0, Data.Length);
+            ContentType = contentType;
+        }
+
+        public StoredFile(string path, Stream data, string contentType) : this(Path.GetFileNameWithoutExtension(path), Path.GetExtension(path), data, contentType) { }
+
+        internal string GetPath()
+        {
+            return $"{Name}{Ext}";
+        }
+    }
+}
