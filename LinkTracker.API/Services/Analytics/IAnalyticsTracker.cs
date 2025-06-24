b85d64d719d51ce5123c62586899deb6e300c07d
Isaac Robinson <isaac@irobinson.org>
2025-06-24 21:28:42 +1000

    Added documentation to interface services

--- LinkTracker.API/Services/Analytics/IAnalyticsTracker.cs
+++ LinkTracker.API/Services/Analytics/IAnalyticsTracker.cs
@@ -2,8 +2,24 @@
 {
     public interface IAnalyticsTracker
     {
+        /// <summary>
+        /// Records a visit to a given filename with an optional referralId.
+        /// </summary>
+        /// <param name="filename">Filename to record the visit to</param>
+        /// <param name="referralId">Optional referralId to record with the visit</param>
         public Task RecordVisit(string filename, string? referralId = null);
+
+        /// <summary>
+        /// Returns all visits in order of UTC time of visit.
+        /// </summary>
+        /// <returns>Enumerable list of Visit objects in order of time of visit</returns>
         public Task<IEnumerable<Visit>> GetVisits();
+
+        /// <summary>
+        /// Returns all visits in order of UTC time of visit.
+        /// </summary>
+        /// <param name="options">Options to apply to limit analytics to be returned</param>
+        /// <returns>Enumerable list of Visit objects in order of time of visit, filtered by the options parameter</returns>
         public Task<IEnumerable<Visit>> GetVisits(Action<AnalyticsOptions> options);
     }
 }
--- LinkTracker.API/Services/FileStorage/IFileStorage.cs
+++ LinkTracker.API/Services/FileStorage/IFileStorage.cs
@@ -4,7 +4,18 @@ namespace LinkTracker.API.Services.FileStorage
 {
     public interface IFileStorage
     {
+        /// <summary>
+        /// Uploads the provided file.
+        /// </summary>
+        /// <param name="file">File to be uploaded</param>
+        /// <returns>Boolean if the upload was successful</returns>
         public Task<bool> UploadFile(StoredFile file);
+
+        /// <summary>
+        /// Attempts to return a file with a matching filename
+        /// </summary>
+        /// <param name="filename">Filename to attempt to fetch</param>
+        /// <returns>Returns a StoredFile object that matches the given filename, returns null if no such file exists</returns>
         public Task<StoredFile?> GetFile(string filename);
     }
 }
