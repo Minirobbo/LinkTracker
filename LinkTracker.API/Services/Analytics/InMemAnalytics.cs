cdb89b21b09ee96aed6c4195e49799c774795089
Isaac Robinson <isaac@irobinson.org>
2025-07-18 00:00:25 +1000

    Added optional testing values for InMemAnalytics

--- LinkTracker.API/Services/Analytics/InMemAnalytics.cs
+++ LinkTracker.API/Services/Analytics/InMemAnalytics.cs
@@ -6,6 +6,16 @@ namespace LinkTracker.API.Services.Analytics
     public class InMemAnalytics : IAnalyticsTracker
     {
         private Dictionary<string, List<Visit>> visits = [];
+        //Testing code:
+        //private Dictionary<string, List<Visit>> visits = new() {
+        //    {"abc", [new Visit("abc", new DateTime(2025, 7, 8)),
+        //             new Visit("abc", new DateTime(2025, 7, 9)),
+        //             new Visit("abc", new DateTime(2025, 7, 12)),
+        //             new Visit("abc", new DateTime(2025, 7, 12)),
+        //             new Visit("abc", new DateTime(2025, 7, 13)),
+        //             new Visit("abc", new DateTime(2025, 7, 14)),
+        //             new Visit("abc", new DateTime(2025, 7, 18))] }
+        //};
 
         public async Task<IEnumerable<Visit>> GetVisits() => await GetVisits(new());
 
